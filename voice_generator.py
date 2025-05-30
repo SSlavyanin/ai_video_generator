5cccf9e548c2aa93d09b970e744d1f0a19a4fe00
SSlavyanin <156539470+SSlavyanin@users.noreply.github.com>
2025-05-30 21:40:41 +0300

    Update voice_generator.py

--- voice_generator.py
+++ voice_generator.py
@@ -1,11 +1,14 @@
-from gtts import gTTS
 import os
+from gtts import gTTS
 
 def generate_voices(phrases: list[str]) -> list[str]:
+    os.makedirs("assets", exist_ok=True)  # Убедимся, что папка существует
+
     paths = []
     for i, phrase in enumerate(phrases):
-        tts = gTTS(phrase, lang='ru')
+        tts = gTTS(text=phrase, lang="ru")
         path = f"assets/voice_{i}.mp3"
         tts.save(path)
+        print(f"Сохранён голос: {path}")
         paths.append(path)
     return paths
